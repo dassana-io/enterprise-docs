185a7179251c4d59bcdb773937ab15c45ea2b9c1
Parth Shah <parth@dassana.io>
2022-05-02 23:39:10 -0700

    ran prettier

--- src/pages/jummie.tsx
+++ src/pages/jummie.tsx
@@ -3,19 +3,19 @@ import React from 'react'
 import styles from './index.module.css'
 
 export default function Jummie() {
-	return (
-		<BrowserOnly fallback={<></>}>
-			{() => {
-				const Plausible =
-					// eslint-disable-next-line @typescript-eslint/no-var-requires
-					require('@dassana-io/web-components').Plausible
+    return (
+        <BrowserOnly fallback={<></>}>
+            {() => {
+                const Plausible =
+                    // eslint-disable-next-line @typescript-eslint/no-var-requires
+                    require('@dassana-io/web-components').Plausible
 
-				return (
-					<div className={styles.jummie}>
-						<Plausible />
-					</div>
-				)
-			}}
-		</BrowserOnly>
-	)
+                return (
+                    <div className={styles.jummie}>
+                        <Plausible />
+                    </div>
+                )
+            }}
+        </BrowserOnly>
+    )
 }
